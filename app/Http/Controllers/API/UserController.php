4f9c0f711f3b09a8077f4258df7e8bd00e0df21c
thai-luu <thailuu0928@gmail.com>
2021-11-21 22:03:07 +0700

    fix

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User; 
 use Illuminate\Support\Facades\Auth; 
 use Validator;
+use DB;
 class UserController extends Controller 
 {
 public $successStatus = 200;
@@ -45,7 +46,9 @@ public $successStatus = 200;
      * @return \Illuminate\Http\Response 
      */ 
     public function register(Request $request) 
-    { 
+    {  
+        
+      
         $validator = Validator::make($request->all(), [ 
             'name' => 'required', 
             'email' => 'required|email', 
@@ -55,12 +58,18 @@ public $successStatus = 200;
 if ($validator->fails()) { 
             return response()->json(['error'=>$validator->errors()], 401);            
         }
-$input = $request->all(); 
+$input = $request->all();
+$check = DB::table('users')->where('email', $input['email'])->first();
+if(!empty($check)){
+    return response()->json(['error'=>"email đã tồn tại"], 401);
+}
+else{
         $input['password'] = bcrypt($input['password']); 
         $user = User::create($input); 
         $success['token'] =  $user->createToken('MyApp')-> accessToken; 
         $success['name'] =  $user->name;
 return response()->json(['success'=>$success], $this-> successStatus); 
+}
     }
 /** 
      * details api 
