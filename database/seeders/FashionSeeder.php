f7b36d2f7c7820e83cf867c01e522473ce8e403f
Hoàng Lê <lehuyhoang0101@gmail.com>
2021-11-28 16:09:17 +0700

    insert data

--- database/seeders/FashionSeeder.php
+++ database/seeders/FashionSeeder.php
@@ -15,28 +15,328 @@ class FashionSeeder extends Seeder
     {
         DB::table('fashions')->insert([
             [
-            'name' => "Áo sơ mi lacoste",
-            'species' => "áo sơ mi",     // chủng loại 
+            'name' => "ラコステシャツ",
+            'species' => "シャツ",     // chủng loại 
             'img_url'=> "https://anhdepfree.com/wp-content/uploads/2018/08/bo-hinh-nen-may-tinh-4k-cuc-dep-20.jpg", // link ảnh
-            'description' => "Áo sơ mi của hãng lacoste chính hãng",
+            'description' => "ラコステ",
             'sold'=> "100", // số lượng đã bán
             'price' => "1000", // giá cả
+            'size' => "M",
         ],
         [
-            'name' => "Áo sơ mi luiviton",
-            'species' => "áo sơ mi", // chủng loại 
+            'name' => "ラコステシャツ",
+            'species' => "シャツ",     // chủng loại 
             'img_url'=> "https://anhdepfree.com/wp-content/uploads/2018/08/bo-hinh-nen-may-tinh-4k-cuc-dep-20.jpg", // link ảnh
-            'description' => "Áo sơ mi của hãng luiviton chính hãng",
+            'description' => "ラコステ",
             'sold'=> "100", // số lượng đã bán
             'price' => "1000", // giá cả
+            'size' => "L",
         ],
         [
-            'name' => "Áo sơ mi gucci",
-            'species' => "áo sơ mi", // chủng loại 
+            'name' => "ラコステシャツ",
+            'species' => "シャツ",     // chủng loại 
             'img_url'=> "https://anhdepfree.com/wp-content/uploads/2018/08/bo-hinh-nen-may-tinh-4k-cuc-dep-20.jpg", // link ảnh
-            'description' => "Áo sơ mi của hãng gucci chính hãng",
+            'description' => "ラコステ",
             'sold'=> "100", // số lượng đã bán
             'price' => "1000", // giá cả
+            'size' => "XL",
+        ],
+        [
+            'name' => "ルイ・ヴィトンシャツ",
+            'species' => "シャツ", // chủng loại 
+            'img_url'=> "https://anhdepfree.com/wp-content/uploads/2018/08/bo-hinh-nen-may-tinh-4k-cuc-dep-20.jpg", // link ảnh
+            'description' => "ルイ・ヴィトン",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1500", // giá cả
+            'size' => "M",
+        ],
+        [
+            'name' => "ルイ・ヴィトンシャツ",
+            'species' => "シャツ", // chủng loại 
+            'img_url'=> "https://anhdepfree.com/wp-content/uploads/2018/08/bo-hinh-nen-may-tinh-4k-cuc-dep-20.jpg", // link ảnh
+            'description' => "ルイ・ヴィトン",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1500", // giá cả
+            'size' => "L",
+        ],
+        [
+            'name' => "ルイ・ヴィトンシャツ",
+            'species' => "シャツ", // chủng loại 
+            'img_url'=> "https://anhdepfree.com/wp-content/uploads/2018/08/bo-hinh-nen-may-tinh-4k-cuc-dep-20.jpg", // link ảnh
+            'description' => "ルイ・ヴィトン",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1500", // giá cả
+            'size' => "XL",
+        ],
+        [
+            'name' => "グッチシャツ",
+            'species' => "シャツ", // chủng loại 
+            'img_url'=> "https://anhdepfree.com/wp-content/uploads/2018/08/bo-hinh-nen-may-tinh-4k-cuc-dep-20.jpg", // link ảnh
+            'description' => "グッチ",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1200", // giá cả
+            'size' => "M",
+        ],
+        [
+            'name' => "グッチシャツ",
+            'species' => "シャツ", // chủng loại 
+            'img_url'=> "https://anhdepfree.com/wp-content/uploads/2018/08/bo-hinh-nen-may-tinh-4k-cuc-dep-20.jpg", // link ảnh
+            'description' => "グッチ",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1200", // giá cả
+            'size' => "L",
+        ],
+        [
+            'name' => "グッチシャツ",
+            'species' => "シャツ", // chủng loại 
+            'img_url'=> "https://anhdepfree.com/wp-content/uploads/2018/08/bo-hinh-nen-may-tinh-4k-cuc-dep-20.jpg", // link ảnh
+            'description' => "グッチ",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1200", // giá cả
+            'size' => "XL",
+        ],
+        [
+            'name' => "グッチセーター",
+            'species' => "セーター", // chủng loại 
+            'img_url'=> "https://media.bizwebmedia.net/Sites/127046/data/images/2018/10/4852584ao_len_gucci_gg_cardigan_chuan_authentic.jpg?0", // link ảnh
+            'description' => "グッチ",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "2000", // giá cả
+            'size' => "M",
+        ],
+        [
+            'name' => "グッチセーター",
+            'species' => "セーター", // chủng loại 
+            'img_url'=> "https://media.bizwebmedia.net/Sites/127046/data/images/2018/10/4852584ao_len_gucci_gg_cardigan_chuan_authentic.jpg?0", // link ảnh
+            'description' => "グッチ",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "2000", // giá cả
+            'size' => "L",
+        ],
+        [
+            'name' => "グッチセーター",
+            'species' => "セーター", // chủng loại 
+            'img_url'=> "https://media.bizwebmedia.net/Sites/127046/data/images/2018/10/4852584ao_len_gucci_gg_cardigan_chuan_authentic.jpg?0", // link ảnh
+            'description' => "グッチ",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "2000", // giá cả
+            'size' => "XL",
+        ],
+        [
+            'name' => "ユニクロセーター",
+            'species' => "セーター", // chủng loại 
+            'img_url'=> "http://product.hstatic.net/1000209952/product/ao-len-cashmere-nam-uniqlo-32_419200_77ae069236504a58a5e7069af5f591f6_grande.jpg", // link ảnh
+            'description' => "ユニクロ",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1000", // giá cả
+            'size' => "M",
+        ],
+        [
+            'name' => "ユニクロセーター",
+            'species' => "セーター", // chủng loại 
+            'img_url'=> "http://product.hstatic.net/1000209952/product/ao-len-cashmere-nam-uniqlo-32_419200_77ae069236504a58a5e7069af5f591f6_grande.jpg", // link ảnh
+            'description' => "ユニクロ",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1000", // giá cả
+            'size' => "L",
+        ],
+        [
+            'name' => "ユニクロセーター",
+            'species' => "セーター", // chủng loại 
+            'img_url'=> "http://product.hstatic.net/1000209952/product/ao-len-cashmere-nam-uniqlo-32_419200_77ae069236504a58a5e7069af5f591f6_grande.jpg", // link ảnh
+            'description' => "ユニクロ",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1000", // giá cả
+            'size' => "XL",
+        ],
+        [
+            'name' => "ユニクロパーカー",
+            'species' => "パーカー", // chủng loại 
+            'img_url'=> "https://cf.shopee.vn/file/f8f2ebcdb3bfeb1e1aa11fb5a8c6aab0", // link ảnh
+            'description' => "ユニクロ",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "800", // giá cả
+            'size' => "M",
+        ],
+        [
+            'name' => "ユニクロパーカー",
+            'species' => "パーカー", // chủng loại 
+            'img_url'=> "https://cf.shopee.vn/file/f8f2ebcdb3bfeb1e1aa11fb5a8c6aab0", // link ảnh
+            'description' => "ユニクロ",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "800", // giá cả
+            'size' => "L",
+        ],
+        [
+            'name' => "ユニクロパーカー",
+            'species' => "パーカー", // chủng loại 
+            'img_url'=> "https://cf.shopee.vn/file/f8f2ebcdb3bfeb1e1aa11fb5a8c6aab0", // link ảnh
+            'description' => "ユニクロ",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "800", // giá cả
+            'size' => "XL",
+        ],
+        [
+            'name' => "アディダススポーツウェア",
+            'species' => "スポーツウェア", // chủng loại 
+            'img_url'=> "https://vn-live-05.slatic.net/p/ffe465da2028b0daf2f295904f1f4619.jpg_720x720q80.jpg_.webp", // link ảnh
+            'description' => "アディダス",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1200", // giá cả
+            'size' => "M",
+        ],
+        [
+            'name' => "アディダススポーツウェア",
+            'species' => "スポーツウェア", // chủng loại 
+            'img_url'=> "https://vn-live-05.slatic.net/p/ffe465da2028b0daf2f295904f1f4619.jpg_720x720q80.jpg_.webp", // link ảnh
+            'description' => "アディダス",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1200", // giá cả
+            'size' => "L",
+        ],
+        [
+            'name' => "アディダススポーツウェア",
+            'species' => "スポーツウェア", // chủng loại 
+            'img_url'=> "https://vn-live-05.slatic.net/p/ffe465da2028b0daf2f295904f1f4619.jpg_720x720q80.jpg_.webp", // link ảnh
+            'description' => "アディダス",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1200", // giá cả
+            'size' => "XL",
+        ],
+        [
+            'name' => "リーバイスジーンズ",
+            'species' => "ジーンズ", // chủng loại 
+            'img_url'=> "https://saigonnewstyles.com/public/cloud/common/SanPham/QUAN-JEAN-LEVIS-514-0742-STRAIGHT-FIT/2.jpg", // link ảnh
+            'description' => "リーバイス",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1400", // giá cả
+            'size' => "M",
+        ],
+        [
+            'name' => "リーバイスジーンズ",
+            'species' => "ジーンズ", // chủng loại 
+            'img_url'=> "https://saigonnewstyles.com/public/cloud/common/SanPham/QUAN-JEAN-LEVIS-514-0742-STRAIGHT-FIT/2.jpg", // link ảnh
+            'description' => "リーバイス",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1400", // giá cả
+            'size' => "L",
+        ],
+        [
+            'name' => "リーバイスジーンズ",
+            'species' => "ジーンズ", // chủng loại 
+            'img_url'=> "https://saigonnewstyles.com/public/cloud/common/SanPham/QUAN-JEAN-LEVIS-514-0742-STRAIGHT-FIT/2.jpg", // link ảnh
+            'description' => "リーバイス",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1400", // giá cả
+            'size' => "XL",
+        ],
+        [
+            'name' => "チャンピオンライフジャケット",
+            'species' => "ライフジャケット", // chủng loại 
+            'img_url'=> "https://cf.shopee.vn/file/b609adda474b82566738b6a8c6bf610c", // link ảnh
+            'description' => "チャンピオン",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1800", // giá cả
+            'size' => "M",
+        ],
+        [
+            'name' => "チャンピオンライフジャケット",
+            'species' => "ライフジャケット", // chủng loại 
+            'img_url'=> "https://cf.shopee.vn/file/b609adda474b82566738b6a8c6bf610c", // link ảnh
+            'description' => "チャンピオン",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1800", // giá cả
+            'size' => "L",
+        ],
+        [
+            'name' => "チャンピオンライフジャケット",
+            'species' => "ライフジャケット", // chủng loại 
+            'img_url'=> "https://cf.shopee.vn/file/b609adda474b82566738b6a8c6bf610c", // link ảnh
+            'description' => "チャンピオン",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "1800", // giá cả
+            'size' => "XL",
+        ],
+        [
+            'name' => "チャンピオンセーター",
+            'species' => "セーター", // chủng loại 
+            'img_url'=> "https://vn-live-05.slatic.net/original/e97fae894702714199ef001c85299c3a.jpg_720x720q80.jpg_.webp", // link ảnh
+            'description' => "チャンピオン",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "800", // giá cả
+            'size' => "M",
+        ],
+        [
+            'name' => "チャンピオンセーター",
+            'species' => "セーター", // chủng loại 
+            'img_url'=> "https://vn-live-05.slatic.net/original/e97fae894702714199ef001c85299c3a.jpg_720x720q80.jpg_.webp", // link ảnh
+            'description' => "チャンピオン",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "800", // giá cả
+            'size' => "L",
+        ],
+        [
+            'name' => "チャンピオンセーター",
+            'species' => "セーター", // chủng loại 
+            'img_url'=> "https://vn-live-05.slatic.net/original/e97fae894702714199ef001c85299c3a.jpg_720x720q80.jpg_.webp", // link ảnh
+            'description' => "チャンピオン",
+            'sold'=> "100", // số lượng đã bán
+            'price' => "800", // giá cả
+            'size' => "XL",
+        ],
+        [
+            'name' => "ナイキジョガーパンツ",
+            'species' => "ジョガーパンツ", // chủng loại 
+            'img_url'=> "https://thesneakerhouse.com/wp-content/uploads/2019/12/Men_s-Swoosh-Logo1.jpg", // link ảnh
+            'description' => "ナイキ",
+            'sold'=> "90", // số lượng đã bán
+            'price' => "1100", // giá cả
+            'size' => "M",
+        ],
+        [
+            'name' => "ナイキジョガーパンツ",
+            'species' => "ジョガーパンツ", // chủng loại 
+            'img_url'=> "https://thesneakerhouse.com/wp-content/uploads/2019/12/Men_s-Swoosh-Logo1.jpg", // link ảnh
+            'description' => "ナイキ",
+            'sold'=> "150", // số lượng đã bán
+            'price' => "1100", // giá cả
+            'size' => "L",
+        ],
+        [
+            'name' => "ナイキジョガーパンツ",
+            'species' => "ジョガーパンツ", // chủng loại 
+            'img_url'=> "https://thesneakerhouse.com/wp-content/uploads/2019/12/Men_s-Swoosh-Logo1.jpg", // link ảnh
+            'description' => "ナイキ",
+            'sold'=> "110", // số lượng đã bán
+            'price' => "1100", // giá cả
+            'size' => "XL",
+        ],
+        [
+            'name' => "プーマTシャツ",
+            'species' => "Tシャツ", // chủng loại 
+            'img_url'=> "https://product.hstatic.net/1000008082/product/45_239fa4a09c93407fa5d63f7a4ff8251e_master.jpeg", // link ảnh
+            'description' => "プーマ",
+            'sold'=> "200", // số lượng đã bán
+            'price' => "500", // giá cả
+            'size' => "M",
+        ],
+        [
+            'name' => "プーマTシャツ",
+            'species' => "Tシャツ", // chủng loại 
+            'img_url'=> "https://product.hstatic.net/1000008082/product/45_239fa4a09c93407fa5d63f7a4ff8251e_master.jpeg", // link ảnh
+            'description' => "プーマ",
+            'sold'=> "200", // số lượng đã bán
+            'price' => "500", // giá cả
+            'size' => "L",
+        ],
+        [
+            'name' => "プーマTシャツ",
+            'species' => "Tシャツ", // chủng loại 
+            'img_url'=> "https://product.hstatic.net/1000008082/product/45_239fa4a09c93407fa5d63f7a4ff8251e_master.jpeg", // link ảnh
+            'description' => "プーマ",
+            'sold'=> "200", // số lượng đã bán
+            'price' => "500", // giá cả
+            'size' => "XL",
         ],
         ]
     );
