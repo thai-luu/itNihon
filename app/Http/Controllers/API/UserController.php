32f394e274f738ad408982d711a65935dde24a87
thai-luu <thailuu0928@gmail.com>
2021-12-15 11:01:35 +0700

    fix recomend

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -116,14 +116,16 @@ return response()->json(['success'=>$success], $this-> successStatus);
             $sex = '%'. $sex .'%';
             
             if($user['age'] >= 18 && $user['age'] <= 25){
-                $fashion = DB::table('fashions')->where(function ($query) use ($sex) {$query->where('sex','like', $sex)->orWhere('sex','=',2);})->paginate(8);
+                $fashion = DB::table('fashions')->where('style', 0)->where(function ($query) use ($sex) {$query->where('sex','like', $sex)->orWhere('sex','=',2);})->paginate(8);
             }
             else if($user['age'] >= 26 && $user['age'] <= 35){
                 
                 $fashion = DB::table('fashions')->where('style', 1)->where(function ($query) use ($sex) {$query->where('sex','like', $sex)->orWhere('sex','=',2);})->paginate(8);
+                
             }
             else if($user['age'] >= 36){
                 $fashion = DB::table('fashions')->where('style', 2)->where(function ($query) use ($sex) {$query->where('sex','like', $sex)->orWhere('sex','=',2);})->paginate(8);
+               
             }
             else{
                 $fashion = DB::table('fashions')->where('sex','like', $sex)->where(function ($query) use ($sex) {$query->where('sex','like', $sex)->orWhere('sex','=',2);})->paginate(8);
