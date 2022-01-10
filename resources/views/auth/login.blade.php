427d24cedb1d05ded4df2a36ab6e93298bb699c6
thai-luu <thailuu0928@gmail.com>
2022-01-10 22:16:09 +0700

    dsfs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,13 +4,13 @@
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
         <div class="panel panel-default">
-            <div class="panel-heading">Đăng nhập</div>
+            <div class="panel-heading">Sign in</div>
             <div class="panel-body">
                 <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
                     {{ csrf_field() }}
 
                     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <label for="email" class="col-md-4 control-label">Địa chỉ email</label>
+                        <label for="email" class="col-md-4 control-label">Email</label>
 
                         <div class="col-md-6">
                             <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -24,7 +24,7 @@
                     </div>
 
                     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <label for="password" class="col-md-4 control-label">Mật khẩu</label>
+                        <label for="password" class="col-md-4 control-label">Password</label>
 
                         <div class="col-md-6">
                             <input id="password" type="password" class="form-control" name="password" required>
@@ -41,7 +41,7 @@
                         <div class="col-md-6 col-md-offset-4">
                             <div class="checkbox">
                                 <label>
-                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Ghi nhớ mật khẩu
+                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember
                                 </label>
                             </div>
                         </div>
@@ -50,12 +50,10 @@
                     <div class="form-group">
                         <div class="col-md-8 col-md-offset-4">
                             <button type="submit" class="btn btn-primary">
-                                Đăng nhập
+                                Login
                             </button>
 
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                                Quên mật khẩu?
-                            </a>
+                            
                         </div>
                     </div>
                 </form>
